f704bd8a69dc204bc953f7c688a76f84acb583c0
pradeep singh <pradeep1991singh@gmail.com>
2017-06-14 15:42:00 +0530

    push notification using firebase cloud messaging
    Signed-off-by: pradeep singh <pradeep1991singh@gmail.com>

--- app/home/home.js
+++ app/home/home.js
@@ -10,7 +10,14 @@ import {
 import { StackNavigator } from 'react-navigation'
 import { Icon, Avatar } from 'react-native-elements'
 import { View as AnimationView } from 'react-native-animatable'
-import OneSignal from 'react-native-onesignal'
+import { Platform } from 'react-native'
+import FCM, {
+  FCMEvent,
+  RemoteNotificationResult,
+  WillPresentNotificationResult,
+  NotificationType,
+} from 'react-native-fcm'
+// import OneSignal from 'react-native-onesignal'
 
 import Bubbles from './bubbles'
 import User from './user'
@@ -65,8 +72,9 @@ export class HomeScreenDrawer extends Component {
 
   componentWillMount() {
     // push notification events
-    OneSignal.addEventListener('opened', this.onOpened)
-    OneSignal.addEventListener('ids', this.onIds)
+    // OneSignal.addEventListener('opened', this.onOpened)
+    // OneSignal.addEventListener('ids', this.onIds)
+    this.registerFCM()
 
     // load data for home screen
     this.props.loadUserInfo()
@@ -91,8 +99,52 @@ export class HomeScreenDrawer extends Component {
   }
 
   componentWillUnmount() {
-    OneSignal.removeEventListener('opened', this.onOpened)
-    OneSignal.removeEventListener('ids', this.onIds)
+    // OneSignal.removeEventListener('opened', this.onOpened)
+    // OneSignal.removeEventListener('ids', this.onIds)
+    // stop listening for events
+    this.notificationListener.remove()
+    this.refreshTokenListener.remove()
+  }
+
+  registerFCM() {
+    FCM.requestPermissions() // for iOS
+    FCM.getFCMToken().then(token => {
+      console.log('token =>>>>', token)
+      // store fcm token in your server
+    })
+
+    this.notificationListener = FCM.on(FCMEvent.Notification, async notif => {
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+      if (notif.local_notification) {
+        //this is a local notification
+      }
+      if (notif.opened_from_tray) {
+        //app is open/resumed because user clicked banner
+      }
+      await someAsyncCall()
+
+      if (Platform.OS === 'ios') {
+        //optional
+        //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
+        //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
+        //notif._notificationType is available for iOS platfrom
+        switch (notif._notificationType) {
+          case NotificationType.Remote:
+            notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
+            break
+          case NotificationType.NotificationResponse:
+            notif.finish()
+            break
+          case NotificationType.WillPresent:
+            notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
+            break
+        }
+      }
+    })
+    this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
+      console.log(token)
+      // fcm token may not be available on first load, catch it here
+    })
   }
 
   handleSwipe = isSwiping => {
