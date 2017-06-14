6845e1458cc4dbd94f444fb72e94966a9d379b21
pradeep singh <pradeep1991singh@gmail.com>
2017-06-14 17:14:29 +0530

    removed unused method and code blocks
    Signed-off-by: pradeep singh <pradeep1991singh@gmail.com>

--- app/home/home.js
+++ app/home/home.js
@@ -109,41 +109,18 @@ export class HomeScreenDrawer extends Component {
   registerFCM() {
     FCM.requestPermissions() // for iOS
     FCM.getFCMToken().then(token => {
-      console.log('token =>>>>', token)
+      console.log('token ===>', token)
       // store fcm token in your server
     })
 
     this.notificationListener = FCM.on(FCMEvent.Notification, async notif => {
-      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
-      if (notif.local_notification) {
-        //this is a local notification
-      }
-      if (notif.opened_from_tray) {
-        //app is open/resumed because user clicked banner
-      }
-      await someAsyncCall()
-
-      if (Platform.OS === 'ios') {
-        //optional
-        //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
-        //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-        //notif._notificationType is available for iOS platfrom
-        switch (notif._notificationType) {
-          case NotificationType.Remote:
-            notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
-            break
-          case NotificationType.NotificationResponse:
-            notif.finish()
-            break
-          case NotificationType.WillPresent:
-            notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
-            break
-        }
+      if (notif.type === 'auth-req') {
+        //redirection
       }
     })
+
     this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
       console.log(token)
-      // fcm token may not be available on first load, catch it here
     })
   }
 
