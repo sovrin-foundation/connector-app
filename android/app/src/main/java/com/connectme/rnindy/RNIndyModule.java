321f33d24d727fb8f58f4dd1481b83f087bebc62
Khagesh Sharma <khageshhiet@gmail.com>
2018-04-23 16:31:43 +0530

    Android: Add Java wrapper for Indy calls (#393)
    ## Reason for Pull Request
    
    - [X] Feature/Change request
    
    ## Link for jira ticket
    [CO-1153](http://evernym.atlassian.net/browse/CO-1153)
    
    ## In case of Feature/Change request
    
    ### High level description of changes done
    
    - Added Indy bridge calls that can be called from JavaScript via promises
    - Added static data to be returned for now, so that we can test end to end flow on Android till receiving proof request
    - Removed app.iml file from source control so that changes of it doesn't get committed again
    
    Signed-off-by: Khagesh Sharma <khageshhiet@gmail.com>

--- android/app/src/main/java/com/connectme/rnindy/RNIndyModule.java
+++ android/app/src/main/java/com/connectme/rnindy/RNIndyModule.java
@@ -4,9 +4,11 @@ package com.connectme.rnindy;
 
 import android.support.annotation.Nullable;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
@@ -16,6 +18,8 @@ import java.util.Map;
 public class RNIndyModule extends ReactContextBaseJavaModule {
     public static final String REACT_CLASS = "RNIndy";
     private static ReactApplicationContext reactContext = null;
+    // TODO:Remove this class once integration with vcx is done
+    private static RNIndyStaticData staticData = new RNIndyStaticData();
 
     public RNIndyModule(ReactApplicationContext context) {
         // Pass in the context to the constructor and save it so you can emit events
@@ -32,25 +36,176 @@ public class RNIndyModule extends ReactContextBaseJavaModule {
         return REACT_CLASS;
     }
 
-    @Override
-    public Map<String, Object> getConstants() {
-        // Export any constants to be used in your native module
-        // https://facebook.github.io/react-native/docs/native-modules-android.html#the-toast-module
-        final Map<String, Object> constants = new HashMap<>();
-        constants.put("EXAMPLE_CONSTANT", "example");
-
-        return constants;
+    @ReactMethod
+    public void addConnection (String remoteDID,
+                            String senderVerificationKey, 
+                            ReadableMap metadata,
+                            Promise promise) {
+        promise.resolve(staticData.addConnection());
     }
 
     @ReactMethod
-    public void exampleMethod () {
-        // An example native method that you will expose to React
-        // https://facebook.github.io/react-native/docs/native-modules-android.html#the-toast-module
+    public void connectToAgency(String url,
+                                String myDid,
+                                String agencyDid,
+                                String myVerKey,
+                                String agencyVerKey,
+                                String poolConfig,
+                                Promise promise) {
+        promise.resolve(staticData.connectAgency());
     }
 
-    private static void emitDeviceEvent(String eventName, @Nullable WritableMap eventData) {
-        // A method for emitting from the native side to JS
-        // https://facebook.github.io/react-native/docs/native-modules-android.html#sending-events-to-javascript
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, eventData);
+    @ReactMethod
+    public void signupWithAgency(String url,
+                                String oneTimeAgencyVerKey,
+                                String oneTimeAgencyDid,
+                                String myOneTimeVerKey,
+                                String agencyVerKey,
+                                String poolConfig,
+                                Promise promise) {
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void createOneTimeAgent(String url,
+                                String oneTimeAgencyVerKey,
+                                String oneTimeAgencyDid,
+                                String myOneTimeVerKey,
+                                String agencyVerKey,
+                                String poolConfig,
+                                Promise promise) {
+        promise.resolve(staticData.oneTimeAgent());
+    }
+
+    @ReactMethod
+    public void createPairwiseAgent(String url,
+                                    String myPairwiseDid,
+                                    String myPairwiseVerKey,
+                                    String oneTimeAgentVerKey,
+                                    String oneTimeAgentDid,
+                                    String myOneTimeVerKey,
+                                    String agencyVerKey,
+                                    String poolConfig,
+                                    Promise promise) {
+        promise.resolve(staticData.pairwiseAgent());
+    }
+
+    @ReactMethod
+    public void acceptInvitation(String url,
+                                String requestId,
+                                String myPairwiseDid,
+                                String myPairwiseVerKey,
+                                ReadableMap invitation,
+                                String myPairwiseAgentDid,
+                                String myPairwiseAgentVerKey,
+                                String myOneTimeAgentDid,
+                                String myOneTimeAgentVerKey,
+                                String myOneTimeDid,
+                                String myOneTimeVerKey,
+                                String myAgencyVerKey,
+                                String poolConfig,
+                                Promise promise) {
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void updatePushToken(String url,
+                                String token,
+                                String uniqueDeviceId,
+                                String myOneTimeAgentDid,
+                                String myOneTimeAgentVerKey,
+                                String myOneTimeVerKey,
+                                String myAgencyVerKey,
+                                String poolConfig,
+                                Promise promise) {
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void getMessage(String url,
+                            String requestId,
+                            String myPairwiseDid,
+                            String myPairwiseVerKey,
+                            String myPairwiseAgentDid,
+                            String myPairwiseAgentVerKey,
+                            String myOneTimeAgentDid,
+                            String myOneTimeAgentVerKey,
+                            String myOneTimeDid,
+                            String myOneTimeVerKey,
+                            String myAgencyVerKey,
+                            String poolConfig,
+                            Promise promise) {
+        promise.resolve(staticData.getMessage());
+    }
+
+    @ReactMethod
+    public void sendMessage(String url,
+                            String messageType,
+                            String messageReplyId,
+                            String message,
+                            String myPairwiseDid,
+                            String myPairwiseVerKey,
+                            String myPairwiseAgentDid,
+                            String myPairwiseAgentVerKey,
+                            String myOneTimeAgentDid,
+                            String myOneTimeAgentVerKey,
+                            String myOneTimeDid,
+                            String myOneTimeVerKey,
+                            String myAgencyVerKey,
+                            String myPairwisePeerVerKey,
+                            String poolConfig,
+                            Promise promise) {
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void deleteConnection(String url,
+                                String myPairwiseDid,
+                                String myPairwiseVerKey,
+                                String myPairwiseAgentDid,
+                                String myPairwiseAgentVerKey,
+                                String myOneTimeAgentDid,
+                                String myOneTimeAgentVerKey,
+                                String myOneTimeDid,
+                                String myOneTimeVerKey,
+                                String myAgencyVerKey,
+                                String poolConfig,
+                                Promise promise) {
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void reset(String poolConfig, Promise promise) {
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void generateClaimRequest(String remoteDid, String claimOffer, String poolConfig, Promise promise) {
+        promise.resolve(staticData.generateClaimRequest());
+    }
+
+    @ReactMethod
+    public void addClaim(String claim, String poolConfig, Promise promise) {
+        promise.resolve("{}");
+    }
+
+    @ReactMethod
+    public void getClaim(String filterJson, String poolConfig, Promise promise) {
+        promise.resolve(staticData.getClaim());
+    }
+
+    @ReactMethod
+    public void prepareProof(String proofRequest, String poolConfig, Promise promise) {
+        promise.resolve("{}");
+    }
+
+    @ReactMethod
+    public void generateProof(String proofRequest,
+                            String remoteDid,
+                            String prepareProof,
+                            String requestedClaims,
+                            String poolConfig,
+                            Promise promise) {
+        promise.resolve("{}");
     }
 }
