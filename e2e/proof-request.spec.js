bc6dc388abace9abfa00990e7db2811f299f7fec
pradeep singh <pradeep1991singh@gmail.com>
2018-08-23 18:17:00 +0530

    Add: e2e test plan stubs [ci skip] (#379)
    ## Reason for Pull Request
    
    - [x] Feature/Change request
    
    ## Tests written for Bug/Feature/Refactoring
    
    - `__e2e__/connection-invitation.spec.js`
    - `__e2e__/claim-offer.spec.js`
    - `__e2e__/proof-request.spec.js`
    - `__e2e__/settings.spec.js`
    - `__e2e__/qr-code.spec.js`
    
    Signed-off-by: pradeep singh <pradeep1991singh@gmail.com>

---
+++ e2e/proof-request.spec.js
@@ -0,0 +1,36 @@
+describe('Proof Request', () => {
+  it('Received Proof Request, Auto-fill, Generate, and Send', async () => {
+    // verify Proof Request popup shows up
+    // verify close button, requested attributes, ignore, and generate button is present
+    // should have 1 attribute value missing
+    // self attest popup should shows up
+    // verify "GENERATE" button is disabled
+    // tap on self attested entry and enter value
+    // verify "GENERATE" button is enabled
+    // tap on generate button
+    // Auto-fill screen should come up
+    // tap on send
+    // verify Continue popup shows up
+    // tap on continue
+    // verify user is redirected to Dashboard screen
+    // tap on Connection bubble from which Proof Request came
+    // verify Connection History popup shows up
+    // verify delete connection, close popup, connection logo, and connection history are present
+    // verify Proof Request "SHARED" entry is present
+    // tap on "SHARED"
+    // verify History Details screen shows up
+    // verify sent attributes is present
+    // verify back button is present
+    // tap on back button
+    // user should come back to connection history popup
+    // tap close button
+    // user should back to dashboard screen
+  })
+
+  it('Received Claim Offer and Ignore', async () => {
+    // verify Proof Request popup shows up
+    // verify close button, attributes, ignore, and accept button is present
+    // tap on Ignore
+    // verify user is redirected to Dashboard screen
+  })
+})
