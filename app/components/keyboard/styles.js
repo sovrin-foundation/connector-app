9f9d44c3df2e402ac8091bdbdb9ea168d0384b4a
tsaicharit <tsaicharit@gmail.com>
2018-10-15 16:28:58 +0530

    Fix: Token keyboard responsive behaviour (#794)
    ## Reason for Pull Request
    
    - [x] Bug fix
    
    ## Link for jira ticket
    [CO-2033](http://evernym.atlassian.net/browse/CO-2033)
    
    - updated the styles for the keyboard - changed the height for android devices accordingly where the key height was cutting in the animation.
    
    Signed-off-by: tsaicharit <tsaicharit@gmail.com>

--- app/components/keyboard/styles.js
+++ app/components/keyboard/styles.js
@@ -10,16 +10,16 @@ import { PUSH_NOTIFICATION_PERMISSION } from '../../push-notification/type-push-
 
 const ROW_MARGIN = isBiggerThanShortDevice ? 36 : 15
 const FIRST_ROW_MARGIN = isBiggerThanShortDevice ? 24 : 8
-// const ROW_HEIGHT = isBiggerThanShortDevice ? 36 : 30
 
 const ROW_HEIGHT =
-  Platform.OS === 'ios' ? 35 : deviceHeight < MEDIUM_DEVICE ? 25 : 35
+  Platform.OS === 'ios' ? 36 : deviceHeight < MEDIUM_DEVICE ? 27 : 36
 
 const FONT_SIZE = isBiggerThanShortDevice ? 28 : 26
 export const INPUT_RANGE = [0, 0.5, 1]
-export const OUTPUT_RANGE = isBiggerThanShortDevice
-  ? [28, 34, 40]
-  : [26, 30, 34]
+export const OUTPUT_RANGE =
+  Platform.OS === 'ios'
+    ? isBiggerThanShortDevice ? [28, 29, 30] : [26, 30, 34]
+    : deviceHeight < MEDIUM_DEVICE ? [28, 30, 32] : [26, 30, 34]
 
 export const styles = StyleSheet.create({
   container: {
