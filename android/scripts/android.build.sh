43addc3c6c355c867db3fe58b37c771574c5fe62
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-12-12 20:38:10 +0530

    Cleaned android build helper script

--- android/scripts/android.build.sh
+++ android/scripts/android.build.sh
@@ -6,10 +6,6 @@ pushd ${SCRIPTPATH}/../../
     yarn install
     react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
     pushd android
-#         pushd keystores
-#             keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
-#         popd
-
         ./gradlew clean build --debug
     popd
 popd
